e461ebf3369e754eba54faf539bb2ffe2faa8176
RyoWakabayashi <rwakabay@oec.co.jp>
2020-07-03 16:18:20 +0900

    docs: シェルの中でyarnを実行する

--- load_dotenv.sh
+++ load_dotenv.sh
@@ -1,9 +1,13 @@
 CURRENT_BRANCH=$(git symbolic-ref --short HEAD)
 
+echo $CURRENT_BRANCH
+
 [[ -f .env ]] && source .env
 
 [[ -f .env.stage && $CURRENT_BRANCH == develop ]] && source .env.stage
 
 [[ -f .env.production && $CURRENT_BRANCH == master ]] && source .env.production
+
+yarn show_direnv_env
 
 exit 0
